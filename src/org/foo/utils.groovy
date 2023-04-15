c134ac816102b6ae4480b4fb1bc708c775b6bcc7
diego <deig0sv@gmail.com>
2023-04-15 14:37:45 -0500

    :hammer: Correccion sintaxis

--- src/org/foo/utils.groovy
+++ src/org/foo/utils.groovy
@@ -3,9 +3,9 @@ package org.foo
 def getInfoResource(Map params = [:]){
     def request = libraryResource "${params.resourceName}"
     def info = readJSON text: request
-    def project = param.project
-    def environment = param.environment
-    def object = param.object
+    def project = params.project
+    def environment = params.environment
+    def object = params.object
 
     return info."${project}"."${environment}"."${object}"
 }
