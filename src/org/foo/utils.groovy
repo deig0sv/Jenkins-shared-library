d01fda0b61818c27107c71a31d9b2d6095a9bb8e
diego <deig0sv@gmail.com>
2023-04-15 15:42:53 -0500

    :hammer: :hammer:

--- src/org/foo/utils.groovy
+++ src/org/foo/utils.groovy
@@ -2,6 +2,7 @@ package org.foo
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
 def getInfoResource(){
-    def request = libraryResource "projects.json"
-    println "${request}"
+    //def request = libraryResource "projects.json"
+    //println "${request}"
+    println "funcion getInfoResource>>>>>>"
 }
