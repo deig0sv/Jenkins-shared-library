888291b38e8377339b21cd49f84d6b61b03b3540
diego <deig0sv@gmail.com>
2023-04-15 15:54:02 -0500

    :hammer: :bug: :hammer:

--- src/org/foo/utils.groovy
+++
@@ -1,8 +0,0 @@
-package org.foo
-import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
-
-def getInfoResource(){
-    //def request = libraryResource "projects.json"
-    //println "${request}"
-    println "funcion getInfoResource>>>>>>"
-}
