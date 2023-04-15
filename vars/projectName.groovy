3e77c9e287051c807b0243ccf10759cdb2f87ba7
diego <deig0sv@gmail.com>
2023-04-15 15:45:47 -0500

    :hammer: :hammer:

--- vars/projectName.groovy
+++ vars/projectName.groovy
@@ -2,11 +2,11 @@ import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 import groovy.transform.Field
 import org.foo.utils
 
-@Field def utils = new utils()
+@Field def utility = new utils()
 
 
 def deploy(Map params = [:]){
-    def charact = utils.getInfoResource()
+    def charact = utility.getInfoResource()
     //println "Desplegando Caracteristica: ${charact}"
     println "Desplegando Caracteristica: ${params.project}"
 }
