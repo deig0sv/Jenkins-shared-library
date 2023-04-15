832d9e802f110e0ade5e7e3bbdbd84c4b5defd5c
diego <deig0sv@gmail.com>
2023-04-15 14:27:43 -0500

    :hammer: Correccion sintaxis

--- vars/projectName.groovy
+++ vars/projectName.groovy
@@ -4,7 +4,7 @@ import org.foo.utils
 @Field def utils = new utils()
 
 
-def deploy(Map params [:]){
+def deploy(Map params = [:]){
     def charact = utils.getInfoResource(
         resourceName: "projects.json",
         project: params.project,
